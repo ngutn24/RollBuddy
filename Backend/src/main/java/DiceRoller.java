2d3717a07dfb2aa0d18676fe0def4814e9ce0921
Arpan <arpanpalsingh3@gmail.com>
2022-04-15 01:18:23 -0700

    Reformatted code

--- Backend/src/main/java/DiceRoller.java
+++ Backend/src/main/java/DiceRoller.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 /**
- * A local dice roller which generates a random number output based on the 
+ * A local dice roller which generates a random number output based on the
  * requested dice type
  */
 
@@ -10,147 +10,147 @@ public class DiceRoller {
     Random dice;
 
     /**
-     * Initialize a DiceRoller object based on the random library 
+     * Initialize a DiceRoller object based on the random library
      */
     public DiceRoller() {
         dice = new Random();
     }
 
     /**
-     * This function represents rolling a D4 (4 sided) dice 
+     * This function represents rolling a D4 (4 sided) dice
      * 
-     * @param diceCount The number of times we want a specific dice to be rolled  
-     * @param modifier The modifer that gets tagged at the end of a "dice roll"
+     * @param diceCount The number of times we want a specific dice to be rolled
+     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
      * 
-     * @return Randomly generated number based on diceCount + the modifier  
+     * @return Randomly generated number based on diceCount + the modifier
      */
     int D4(int diceCount, int modifier) {
         int total = 0;
-        // Roll the dice 
-        while (diceCount != 0 ) {
-            // Generate random number between 1-4 
+        // Roll the dice
+        while (diceCount != 0) {
+            // Generate random number between 1-4
             total += dice.nextInt(4) + 1;
-            diceCount--;  
+            diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
     }
 
     /**
-     * This function represents rolling a D6 (6 sided) dice 
+     * This function represents rolling a D6 (6 sided) dice
      * 
-     * @param diceCount The number of times we want a specific dice to be rolled  
-     * @param modifier The modifer that gets tagged at the end of a "dice roll"
+     * @param diceCount The number of times we want a specific dice to be rolled
+     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
      * 
-     * @return Randomly generated number based on diceCount + the modifier  
+     * @return Randomly generated number based on diceCount + the modifier
      */
     int D6(int diceCount, int modifier) {
         int total = 0;
-        // Roll the dice 
-        while (diceCount != 0 ) {
-            // Generate random number between 1-6 
+        // Roll the dice
+        while (diceCount != 0) {
+            // Generate random number between 1-6
             total += dice.nextInt(6) + 1;
-            diceCount--;  
+            diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
     }
 
     /**
-     * This function represents rolling a D8 (8 sided) dice 
+     * This function represents rolling a D8 (8 sided) dice
      * 
-     * @param diceCount The number of times we want a specific dice to be rolled  
-     * @param modifier The modifer that gets tagged at the end of a "dice roll"
+     * @param diceCount The number of times we want a specific dice to be rolled
+     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
      * 
-     * @return Randomly generated number based on diceCount + the modifier  
+     * @return Randomly generated number based on diceCount + the modifier
      */
     int D8(int diceCount, int modifier) {
         int total = 0;
-        // Roll the dice 
-        while (diceCount != 0 ) {
-            // Generate random number between 1-4 
+        // Roll the dice
+        while (diceCount != 0) {
+            // Generate random number between 1-4
             total += dice.nextInt(8) + 1;
-            diceCount--;  
+            diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
     }
 
     /**
-     * This function represents rolling a D10 (10 sided) dice 
+     * This function represents rolling a D10 (10 sided) dice
      * 
-     * @param diceCount The number of times we want a specific dice to be rolled  
-     * @param modifier The modifer that gets tagged at the end of a "dice roll"
+     * @param diceCount The number of times we want a specific dice to be rolled
+     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
      * 
-     * @return Randomly generated number based on diceCount + the modifier  
+     * @return Randomly generated number based on diceCount + the modifier
      */
     int D10(int diceCount, int modifier) {
         int total = 0;
-        // Roll the dice 
-        while (diceCount != 0 ) {
-            // Generate random number between 1-10 
+        // Roll the dice
+        while (diceCount != 0) {
+            // Generate random number between 1-10
             total += dice.nextInt(10) + 1;
-            diceCount--;  
+            diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
     }
 
     /**
-     * This function represents rolling a D12 (12 sided) dice 
+     * This function represents rolling a D12 (12 sided) dice
      * 
-     * @param diceCount The number of times we want a specific dice to be rolled  
-     * @param modifier The modifer that gets tagged at the end of a "dice roll"
+     * @param diceCount The number of times we want a specific dice to be rolled
+     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
      * 
-     * @return Randomly generated number based on diceCount + the modifier  
+     * @return Randomly generated number based on diceCount + the modifier
      */
     int D12(int diceCount, int modifier) {
         int total = 0;
-        // Roll the dice 
-        while (diceCount != 0 ) {
-            // Generate random number between 1-12 
+        // Roll the dice
+        while (diceCount != 0) {
+            // Generate random number between 1-12
             total += dice.nextInt(12) + 1;
-            diceCount--;  
+            diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
     }
 
     /**
-     * This function represents rolling a D20 (20 sided) dice 
+     * This function represents rolling a D20 (20 sided) dice
      * 
-     * @param diceCount The number of times we want a specific dice to be rolled  
-     * @param modifier The modifer that gets tagged at the end of a "dice roll"
+     * @param diceCount The number of times we want a specific dice to be rolled
+     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
      * 
-     * @return Randomly generated number based on diceCount + the modifier  
+     * @return Randomly generated number based on diceCount + the modifier
      */
     int D20(int diceCount, int modifier) {
         int total = 0;
-        // Roll the dice 
-        while (diceCount != 0 ) {
-            // Generate random number between 1-20 
+        // Roll the dice
+        while (diceCount != 0) {
+            // Generate random number between 1-20
             total += dice.nextInt(20) + 1;
-            diceCount--;  
+            diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
     }
 
     /**
-     * This function represents rolling a D100 (100 sided) dice 
+     * This function represents rolling a D100 (100 sided) dice
      * 
-     * @param diceCount The number of times we want a specific dice to be rolled  
-     * @param modifier The modifer that gets tagged at the end of a "dice roll"
+     * @param diceCount The number of times we want a specific dice to be rolled
+     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
      * 
-     * @return Randomly generated number based on diceCount + the modifier  
+     * @return Randomly generated number based on diceCount + the modifier
      */
     int D100(int diceCount, int modifier) {
         int total = 0;
-        // Roll the dice 
-        while (diceCount != 0 ) {
-            // Generate random number between 1-100 
+        // Roll the dice
+        while (diceCount != 0) {
+            // Generate random number between 1-100
             total += dice.nextInt(100) + 1;
-            diceCount--;  
+            diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
--- Backend/src/main/java/Modifier.java
+++ Backend/src/main/java/Modifier.java
@@ -1,33 +1,35 @@
 /**
- * In Dungeons & Dragon, every character has 6 ability scores (STR, DEX, CON, INT, WIS, CHA)
- * Genreally, the stats range from 1 to 20 but theoretically can be any number 
- * An ability score of 10 is the average, which nets you a modifier of +0 to that ability 
- * Example: 
- *  Strength ability score = 10 
- *  Strength modifier = + 0 
+ * In Dungeons & Dragon, every character has 6 ability scores (STR, DEX, CON,
+ * INT, WIS, CHA)
+ * Genreally, the stats range from 1 to 20 but theoretically can be any number
+ * An ability score of 10 is the average, which nets you a modifier of +0 to
+ * that ability
+ * Example:
+ * Strength ability score = 10
+ * Strength modifier = + 0
  * 
  * Starting from 10, every increment of 2 gives you a +1 to your modifier
  * 
  * Example:
- *  Strength ability score = 15 
- *  Strength modifier = +2 
+ * Strength ability score = 15
+ * Strength modifier = +2
  * 
- *  Strength ability score = 16
- *  Strength modifier = +3 
+ * Strength ability score = 16
+ * Strength modifier = +3
  * 
- * Should your modifier be 9, you get a -1 to your modifier. Every decrement of 2 from 9 gives you a subsequent -1
+ * Should your modifier be 9, you get a -1 to your modifier. Every decrement of
+ * 2 from 9 gives you a subsequent -1
  * 
- * Example: 
- *  Strength ability score = 9
- *  Strength modifier = -1
- *  
- *  Strength ability score = 8
- *  Strength modifier = -1 
+ * Example:
+ * Strength ability score = 9
+ * Strength modifier = -1
  * 
- *  Strength ability score = 7
- *  Stregnth modifier = -2
+ * Strength ability score = 8
+ * Strength modifier = -1
+ * 
+ * Strength ability score = 7
+ * Stregnth modifier = -2
  */
-
 
 public class Modifier {
     final int baseMod = 10;
@@ -37,6 +39,7 @@ public class Modifier {
 
     /**
      * Intialize a Modifier object containing the ability and corresponding modifier
+     * 
      * @param abilityScore Specified ability score
      */
     public Modifier(int abilityScore) {
@@ -45,23 +48,26 @@ public class Modifier {
     }
 
     /**
-     * Take an ability score input and mathematically calculate the associated modifier 
-     * @param abilityScore The ability score tied to a specific modifer 
-     * @return Modifier based on ability score 
+     * Take an ability score input and mathematically calculate the associated
+     * modifier
+     * 
+     * @param abilityScore The ability score tied to a specific modifer
+     * @return Modifier based on ability score
      */
     private int generateModifier(int abilityScore) {
         // Calculate how far from baseMod the abilityScore is
         int deviation = abilityScore -= baseMod;
 
         if (deviation >= 0) {
-            return deviation/2;
+            return deviation / 2;
         } else {
-            return (deviation+1)/2 - 1;
+            return (deviation + 1) / 2 - 1;
         }
     }
 
     /**
-     * Set the new ability score and calculate the new modifier 
+     * Set the new ability score and calculate the new modifier
+     * 
      * @param abilityScore The new ability score
      */
     void setAbilityScore(int abilityScore) {
@@ -70,14 +76,14 @@ public class Modifier {
     }
 
     /**
-     * Get the ability score 
+     * Get the ability score
      */
     int getAbilityScore() {
         return this.abilityScore;
     }
 
     /**
-     * Get the modifier 
+     * Get the modifier
      */
     int getMod() {
         return this.mod;
