5aa7902c70ddc75266f29c9b63e6c6253f7a9d6f
junhu <jxhu@uw.edu>
2022-04-19 09:34:42 -0700

    testing react and adding some text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
+const Character = (props) => {
+    return (
+        <>
+            <h1>Name: {props.name}</h1>
+            <h1>Strength: {props.strength}</h1>
+            <h2>Dexterity: {props.dexterity}</h2>
+            <h2>Constitution : {props.constitution}</h2>
+            <h2>Intelligence : {props.intelligence}</h2>
+            <h2>Wisdom : {props.wisdom}</h2>
+            <h2>Charisma : {props.charisma}</h2>
+        </>
+    )
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Character name={"Arnazall"} strength={10} dexterity={15} constitution={5} intelligence={20} wisdom={25}
+                       charisma={3}/>
+
+
+        </div>
+    );
 }
 
 export default App;
