3887a9452ecc6013b030388d5943923dca783d0a
Esau Abraham <esauabraham@Esaus-MacBook-Air.local>
2022-04-27 16:17:44 -0700

    Gradle Migration Complete

--- Backend/src/main/java/Test.java
+++ Backend/src/main/java/Test.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Test {
+package main.java;
+
+public class Test {
+    /***
+     *
+     * @param args The test's proper arguements
+     */
+    public static void main(String[] args){
+        System.out.println("I'm a little tea pop, short and BIG");
+    }
 }
