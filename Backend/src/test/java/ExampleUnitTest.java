7c4abd2f4f295ade84ddf46682d4194e78f9fa2f
Esau Abraham <esauabraham@Esaus-MacBook-Air.local>
2022-04-27 16:17:44 -0700

    Gradle Migration Complete

---
+++ Backend/src/test/java/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package test.java;
+
+import org.junit.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void testInvalidFileName(){
+        System.out.println("Example Test");
+    }
+
+}
