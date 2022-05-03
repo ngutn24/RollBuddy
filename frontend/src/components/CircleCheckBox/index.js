f32b5f6509c5c29b8dc9297f3512e812155e81a5
junhu <jxhu@uw.edu>
2022-05-02 19:49:32 -0700

    checked box and circle interaction

--- frontend/src/components/CircleCheckBox/index.js
+++ frontend/src/components/CircleCheckBox/index.js
@@ -1,8 +1,32 @@
 import React from "react";
-import {CircleCheckBox} from "./CircleCheckBox.style";
-
-const CircleCheckBoxComponent = () => {
-    return <CircleCheckBox/>;
+import styled from "styled-components";
+export const CircleCheckBox1 = ({}) => {
+    return (
+        <CircleCheckBoxRoot>
+            <SelectedFalse
+                src={"https://file.rendit.io/n/LWQ5eQgcaRfy1lVOFzbh.svg"}
+            />
+            <SelectedFalse
+                src={"https://file.rendit.io/n/6R9ujdzFDu0s6zs6jnoR.svg"}
+            />
+        </CircleCheckBoxRoot>
+    );
 };
-
-export default CircleCheckBoxComponent;
+const CircleCheckBoxRoot = styled.div`
+  border-width: 1px;
+  border-color: #7b61ff;
+  border-style: solid;
+  width: 20px;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  gap: 16px;
+  margin: auto;
+  align-items: center;
+  border-radius: 5px;
+  padding: 15px 15px 19px 19px;
+`;
+const SelectedFalse = styled.img`
+  width: 16px;
+  height: 12px;
+`;
--- frontend/src/components/SquareCheckBox/index.js
+++ frontend/src/components/SquareCheckBox/index.js
@@ -1,8 +1,32 @@
 import React from "react";
-import {SquareCheckBox} from "./SquareCheckBox.style";
-
-const SquareCheckBoxComponent = () => {
-    return <SquareCheckBox/>;
+import styled from "styled-components";
+export const SquareCheckBox1 = ({}) => {
+    return (
+        <SquareCheckBoxRoot>
+            <SelectedFalse
+                src={"https://file.rendit.io/n/S9QP1DU5TWueyXqWgeHY.svg"}
+            />
+            <SelectedFalse
+                src={"https://file.rendit.io/n/U8IbQKhM2xx8z23y6Osf.svg"}
+            />
+        </SquareCheckBoxRoot>
+    );
 };
-
-export default SquareCheckBoxComponent;
+const SquareCheckBoxRoot = styled.div`
+  border-width: 1px;
+  border-color: #7b61ff;
+  border-style: solid;
+  width: 44px;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  gap: 8px;
+  margin: auto;
+  align-items: center;
+  border-radius: 5px;
+  padding: 11px 23px 13px 28px;
+`;
+const SelectedFalse = styled.img`
+  width: 40px;
+  height: 36px;
+`;
