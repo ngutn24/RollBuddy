8ec45416992ceb3c426cff3c6fa8873ebc1ff7c4
Arpan <arpanpalsingh3@gmail.com>
2022-05-03 01:28:35 -0700

    Fixing commit

--- Backend/src/main/java/DiceRoller.java
+++ Backend/src/main/java/DiceRoller.java
@@ -17,142 +17,30 @@ public class DiceRoller {
     }
 
     /**
-     * This function represents rolling a D4 (4 sided) dice
+     * This function represents a dice roll. The theoretical range of the number
+     * of sided dice is infinite, so long as it is positive
      * 
      * @param diceCount The number of times we want a specific dice to be rolled
      * @param modifier  The modifer that gets tagged at the end of a "dice roll"
+     * @param DiceType  The type of dice being rolled
      * 
-     * @return Randomly generated number based on diceCount + the modifier
+     * @return Randomly generated number based on diceCount + the modifier + dice
+     *         type
      */
-    int D4(int diceCount, int modifier) {
+    int Roll(int diceCount, int modifier, int DiceType) {
+
+        assert (diceCount > 0) : "The dice count must be positive";
+        assert (DiceType > 0) : "Cannot roll a negative sided dice";
+
         int total = 0;
         // Roll the dice
         while (diceCount != 0) {
             // Generate random number between 1-4
-            total += dice.nextInt(4) + 1;
+            total += dice.nextInt(DiceType) + 1;
             diceCount--;
         }
         // Modifiers get added at the end of all total diceRolls in DnD
         return total + modifier;
     }
 
-    /**
-     * This function represents rolling a D6 (6 sided) dice
-     * 
-     * @param diceCount The number of times we want a specific dice to be rolled
-     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
-     * 
-     * @return Randomly generated number based on diceCount + the modifier
-     */
-    int D6(int diceCount, int modifier) {
-        int total = 0;
-        // Roll the dice
-        while (diceCount != 0) {
-            // Generate random number between 1-6
-            total += dice.nextInt(6) + 1;
-            diceCount--;
-        }
-        // Modifiers get added at the end of all total diceRolls in DnD
-        return total + modifier;
-    }
-
-    /**
-     * This function represents rolling a D8 (8 sided) dice
-     * 
-     * @param diceCount The number of times we want a specific dice to be rolled
-     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
-     * 
-     * @return Randomly generated number based on diceCount + the modifier
-     */
-    int D8(int diceCount, int modifier) {
-        int total = 0;
-        // Roll the dice
-        while (diceCount != 0) {
-            // Generate random number between 1-4
-            total += dice.nextInt(8) + 1;
-            diceCount--;
-        }
-        // Modifiers get added at the end of all total diceRolls in DnD
-        return total + modifier;
-    }
-
-    /**
-     * This function represents rolling a D10 (10 sided) dice
-     * 
-     * @param diceCount The number of times we want a specific dice to be rolled
-     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
-     * 
-     * @return Randomly generated number based on diceCount + the modifier
-     */
-    int D10(int diceCount, int modifier) {
-        int total = 0;
-        // Roll the dice
-        while (diceCount != 0) {
-            // Generate random number between 1-10
-            total += dice.nextInt(10) + 1;
-            diceCount--;
-        }
-        // Modifiers get added at the end of all total diceRolls in DnD
-        return total + modifier;
-    }
-
-    /**
-     * This function represents rolling a D12 (12 sided) dice
-     * 
-     * @param diceCount The number of times we want a specific dice to be rolled
-     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
-     * 
-     * @return Randomly generated number based on diceCount + the modifier
-     */
-    int D12(int diceCount, int modifier) {
-        int total = 0;
-        // Roll the dice
-        while (diceCount != 0) {
-            // Generate random number between 1-12
-            total += dice.nextInt(12) + 1;
-            diceCount--;
-        }
-        // Modifiers get added at the end of all total diceRolls in DnD
-        return total + modifier;
-    }
-
-    /**
-     * This function represents rolling a D20 (20 sided) dice
-     * 
-     * @param diceCount The number of times we want a specific dice to be rolled
-     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
-     * 
-     * @return Randomly generated number based on diceCount + the modifier
-     */
-    int D20(int diceCount, int modifier) {
-        int total = 0;
-        // Roll the dice
-        while (diceCount != 0) {
-            // Generate random number between 1-20
-            total += dice.nextInt(20) + 1;
-            diceCount--;
-        }
-        // Modifiers get added at the end of all total diceRolls in DnD
-        return total + modifier;
-    }
-
-    /**
-     * This function represents rolling a D100 (100 sided) dice
-     * 
-     * @param diceCount The number of times we want a specific dice to be rolled
-     * @param modifier  The modifer that gets tagged at the end of a "dice roll"
-     * 
-     * @return Randomly generated number based on diceCount + the modifier
-     */
-    int D100(int diceCount, int modifier) {
-        int total = 0;
-        // Roll the dice
-        while (diceCount != 0) {
-            // Generate random number between 1-100
-            total += dice.nextInt(100) + 1;
-            diceCount--;
-        }
-        // Modifiers get added at the end of all total diceRolls in DnD
-        return total + modifier;
-    }
 }
