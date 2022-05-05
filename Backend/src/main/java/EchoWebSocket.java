7bca43950a974b32909c2c09b6eae2ee1fb9bbd8
jongwon1-1932256 <jongwon1@uw.edu>
2022-05-05 01:57:15 -0700

    creating, updating, returning character info

---
+++ Backend/src/main/java/EchoWebSocket.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import spark.Spark;
+
+@WebSocket
+public class EchoWebSocket {
+
+    // Store sessions if you want to, for example, broadcast a message to all users
+    private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
+
+    @OnWebSocketConnect
+    public void connected(Session session) {
+        sessions.add(session);
+    }
+
+    @OnWebSocketClose
+    public void closed(Session session, int statusCode, String reason) {
+        sessions.remove(session);
+    }
+
+}
---
+++ Backend/src/main/java/Session.java
@@ -0,0 +1,45 @@
+package main.java;
+
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Session {
+
+    private static Map<String, CharacterSession> sess;
+
+    public static void main(String[] args) {
+        sess = new HashMap<>();
+        /*
+         * creates a session for a character based on stats which will be in the
+         * format of a comma seperated string with STR, DEX, CON, INT, WIS, CHA, Level,
+         * hitPoints,
+         * gold, armor, main class, subclass
+         */
+        post("/character/:stats", (req, res) -> {
+            CharacterSession currSession = new CharacterSession(Integer.parseInt(req.session().id()));
+            currSession.generateCharacter(req.params(":stats"));
+            sess.put(req.session().id(), currSession); // can change the characterSession constructor to String
+            return req.session().id();
+        });
+
+        /*
+         * updates the session's character sheet using the same stats format as creating
+         */
+        post("/update/:stats", (req, res) -> {
+            CharacterSession currSession = sess.get(req.session().id());
+            currSession.generateCharacter(req.params(":stats"));
+            return req.session().id();
+        });
+
+        /*
+         * returns the stats for the current session
+         */
+        get("/character", (req, res) -> {
+            CharacterSession currSession = sess.get(req.session().id());
+            return currSession.getCharacterData();
+        });
+
+    }
+}
