66cd237a2a47274d3a57bfa7790631c682c21f37
jongwon1-1932256 <jongwon1@uw.edu>
2022-05-08 01:06:37 -0700

    Server seems to be working locally

--- Backend/src/main/java/Session.java
+++ Backend/src/main/java/Session.java
@@ -1,30 +1,28 @@
 package main.java;
 
-import static spark.Spark.*;
+import spark.Spark;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Session {
 
-    private static Map<String, CharacterSession> sess;
-
     public static void main(String[] args) {
-
+        
+        Map<String, CharacterSession> sess = new HashMap<>();
         /*
          * For testing purposes. go to http://localhost:4567/hello to see if the server
          * is running
          */
-        get("/hello", (req, res) -> {
+        Spark.get("/hello", (req, res) -> {
             String name = req.queryParams("name");
             return "Hello, " + (name != null ? name : "friend");
         });
 
-        sess = new HashMap<>();
         /*
          * creates a session for a character
          */
-        post("/character/create", (req, res) -> {
+        Spark.get("/create", (req, res) -> {
             CharacterSession currSession = new CharacterSession(req.session().id());
             sess.put(req.session().id(), currSession); // can change the characterSession constructor to String
             return req.session().id();
@@ -36,7 +34,7 @@ public class Session {
          * hitPoints,
          * gold, armor, main class, subclass
          */
-        put("/update/:stats", (req, res) -> {
+        Spark.get("/update/:stats", (req, res) -> {
             CharacterSession currSession = sess.get(req.session().id());
             currSession.generateCharacter(req.params(":stats"));
             return req.session().id();
@@ -45,16 +43,16 @@ public class Session {
         /*
          * returns the stats for the current session
          */
-        get("/character", (req, res) -> {
+        Spark.get("/character", (req, res) -> {
             CharacterSession currSession = sess.get(req.session().id());
             return currSession.getCharacterData();
         });
 
-        get("/roll/:count/:mod/:dice", (req, res) -> {
+        Spark.get("/roll/:count/:mod/:dice", (req, res) -> {
             CharacterSession currSession = sess.get(req.session().id());
             return currSession.rollDice(Integer.parseInt(req.params(":count")), req.params(":mod"),
                     req.params(":dice"));
         });
-
+        //Spark.stop();
     }
 }
