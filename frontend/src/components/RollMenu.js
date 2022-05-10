6035ae335e80e0eae9f4b7323b047e7d1dc8aa8f
Tyler W <19383474+leftl@users.noreply.github.com>
2022-05-09 21:56:41 -0700

    Implement roll menu with API call.

--- frontend/src/components/RollMenu.js
+++ frontend/src/components/RollMenu.js
@@ -1,7 +1,8 @@
 import { React, useState, useEffect } from "react";
 import { DropdownButton, Dropdown, Button, ButtonGroup } from "react-bootstrap";
+import axios from "axios";
 
-var RollWindow = {
+const RollWindow = {
   maxWidth: "25rem",
   outline: "black solid 2px",
   borderRadius: "20px",
@@ -9,27 +10,72 @@ var RollWindow = {
   margin: "1rem 1rem 1rem 1rem",
 };
 
+const RollInputCount = {
+  width: "6rem",
+  height: "4rem",
+  margin: "0rem 0.5rem 0rem 0.5rem",
+  fontSize: "2rem",
+  textAlign: "right",
+};
+
+const abilityCode = {
+  Strength: "str",
+  Dexterity: "dex",
+  Constitution: "con",
+  Intelligence: "int",
+  Wisdom: "wis",
+  Charisma: "cha",
+};
+
 const RollMenu = () => {
+  // Selector states
   const [rollType, setRollType] = useState("Attack");
-  const handleSelectRollType = (e) => {
-    setRollType(e);
-  };
-
   const [rollCondition, setRollCondition] = useState("Normal");
-  const handleSelectRollCondition = (e) => {
-    setRollCondition(e);
-  };
-
-  const [rollAbility, setRollAbility] = useState("Strength");
-  const handleSelectRollAbility = (e) => {
-    setRollAbility(e);
-  };
-
+  const [rollAbility, setRollAbility] = useState("str");
   const [rollProficiency, setRollProficiency] = useState("Proficent");
-  const handleSelectRollProficiency = (e) => {
-    setRollProficiency(e);
+  const [rollDice, setRollDice] = useState("4");
+  // Roller States
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const handleSelectState = (setState) => (e) => {
+    setState(e);
   };
 
+  const handleInputState = (setState) => (e) => {
+    const re = RegExp(/^[0-9\b]+$/);
+    // if blank, set value to 0 (avoids NaN). otherwise, if the input value is a valid int, update states.
+    if (e.target.value === "") {
+      setState(parseInt(0));
+    } else if (re.test(e.target.value)) {
+      setState(parseInt(e.target.value));
+    }
+  };
+
+  const fetchDiceRoll = () => {
+    const url =
+      "http://localhost:4567/roll?mod=" +
+      abilityCode[rollAbility] +
+      "&count=" +
+      count +
+      "&dice=d" +
+      rollDice;
+    axios
+      .get(url, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setValue(res.data);
+        console.log("[DEBUG] Fetched dice roll: ", {
+          mod: rollAbility,
+          count: count,
+          dice: "d" + rollDice,
+          value: res.data,
+        });
+      });
+  };
+
+  // load state from local storage
   useEffect(() => {
     const localRollInfo = JSON.parse(localStorage.getItem("rollInfo"));
     if (localRollInfo) {
@@ -38,18 +84,23 @@ const RollMenu = () => {
       setRollCondition(localRollInfo.rollCondition);
       setRollAbility(localRollInfo.rollAbility);
       setRollProficiency(localRollInfo.rollProficiency);
+      setRollDice(localRollInfo.rollDice);
+      setCount(localRollInfo.count);
     }
   }, []);
 
+  // store state to local storage
   useEffect(() => {
     const rollInfo = {
       rollType: rollType,
       rollCondition: rollCondition,
       rollAbility: rollAbility,
       rollProficiency: rollProficiency,
+      rollDice: rollDice,
+      count: count,
     };
     localStorage.setItem("rollInfo", JSON.stringify(rollInfo));
-  }, [rollType, rollCondition, rollAbility, rollProficiency]);
+  }, [rollType, rollCondition, rollAbility, rollProficiency, rollDice, count]);
 
   return (
     <div align="center" style={RollWindow}>
@@ -57,7 +108,7 @@ const RollMenu = () => {
         <DropdownButton
           id="roll_Type"
           title={rollType}
-          onSelect={handleSelectRollType}
+          onSelect={handleSelectState(setRollType)}
           style={{ margin: "1rem 0.5rem 0.5rem 1rem", width: "100%" }}
         >
           <Dropdown.Item eventKey="Attack">Attack</Dropdown.Item>
@@ -68,7 +119,7 @@ const RollMenu = () => {
         <DropdownButton
           id="roll_Condition"
           title={rollCondition}
-          onSelect={handleSelectRollCondition}
+          onSelect={handleSelectState(setRollCondition)}
           style={{ margin: "1rem 1rem 0.5rem 0.5rem", width: "100%" }}
         >
           <Dropdown.Item eventKey="Normal">Normal</Dropdown.Item>
@@ -80,7 +131,7 @@ const RollMenu = () => {
         <DropdownButton
           id="roll_Ability"
           title={rollAbility}
-          onSelect={handleSelectRollAbility}
+          onSelect={handleSelectState(setRollAbility)}
           style={{ margin: "0.5rem 0.5rem 1rem 1rem" }}
         >
           <Dropdown.Item eventKey="Strength">Strength</Dropdown.Item>
@@ -93,7 +144,7 @@ const RollMenu = () => {
         <DropdownButton
           id="roll_Proficiency"
           title={rollProficiency}
-          onSelect={handleSelectRollProficiency}
+          onSelect={handleSelectState(setRollProficiency)}
           style={{ margin: "0.5rem 1rem 1rem 0.5rem" }}
         >
           <Dropdown.Item eventKey="No">No</Dropdown.Item> {/* +0 */}
@@ -103,14 +154,43 @@ const RollMenu = () => {
           {/* +4 */}
         </DropdownButton>
       </ButtonGroup>
-      <br></br>
-      <p style={{ display: "inline" }}>
+      <div className="roller-inputs" style={{ padding: "0.5rem" }}>
+        <input
+          type="text"
+          value={count}
+          onChange={handleInputState(setCount)}
+          style={RollInputCount}
+        ></input>
+        <p style={{ display: "inline", fontSize: "2rem" }}>d</p>
+
+        <DropdownButton
+          id="roll_Dice"
+          size="lg"
+          variant="secondary"
+          title={rollDice}
+          onSelect={handleSelectState(setRollDice)}
+          style={{ display: "inline", margin: "0rem 0.5rem 0rem 0.5rem" }}
+        >
+          <Dropdown.Item eventKey="4">4</Dropdown.Item>
+          <Dropdown.Item eventKey="6">6</Dropdown.Item>
+          <Dropdown.Item eventKey="8">8</Dropdown.Item>
+          <Dropdown.Item eventKey="10">10</Dropdown.Item>
+          <Dropdown.Item eventKey="12">12</Dropdown.Item>
+          <Dropdown.Item eventKey="20">20</Dropdown.Item>
+          <Dropdown.Item eventKey="100">100</Dropdown.Item>
+        </DropdownButton>
+      </div>
+      {/* <p style={{ display: "inline" }}>
         Constitution Saving Throw: 13 (1d20) + 2 (proficent) + 2 (CON modifier)
         =
-      </p>{" "}
-      {/* Have this display values used for calculation */}
-      <h2>17</h2>
+      </p>{" "} */
+      /* TODO: Have this display values used for calculation */}
+      <div className="roller-output" style={{ padding: "0.5rem" }}>
+        <h4>Roll Value: </h4>
+        <h2>{value}</h2>
+      </div>
       <Button
+        onClick={fetchDiceRoll}
         style={{
           margin: "1rem",
           width: "80%",
