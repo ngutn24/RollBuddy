8a7d891a8aa78ea8c585e5b5344534f81cfcfc3d
Tyler W <19383474+leftl@users.noreply.github.com>
2022-05-09 22:58:24 -0700

    remove unused nav components.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,12 +1,4 @@
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Container,
-  Form,
-  Button,
-  FormControl,
-} from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/rollbuddy_logo.png";
 
 const Navigation = () => {
@@ -26,28 +18,7 @@ const Navigation = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="primary">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
