b19de080711a9b6437f1700f98ed4f6442350412
Tyler W <19383474+leftl@users.noreply.github.com>
2022-05-09 20:18:21 -0700

    Complete ability handling via API.

--- frontend/src/components/AbilityList.js
+++ frontend/src/components/AbilityList.js
@@ -1,35 +1,26 @@
-import { React, useState } from "react";
+import { React } from "react";
 import Ability from "./Ability.js";
 
-const AbilityList = () => {
-  const [abilities, setAbilities] = useState({
-    str: 10,
-    dex: 10,
-    con: 10,
-    int: 10,
-    wis: 10,
-    cha: 10,
-  });
-
+const AbilityList = ({ abilities, setAbilities }) => {
   // Map from key to display name
   const abilityDisplayNames = {
-    str: "Strength",
-    dex: "Dexterity",
-    con: "Constitution",
-    int: "Intelligence",
-    wis: "Wisdom",
-    cha: "Charisma",
+    STR: "Strength",
+    DEX: "Dexterity",
+    CON: "Constitution",
+    INT: "Intelligence",
+    WIS: "Wisdom",
+    CHA: "Charisma",
   };
 
   return (
     <>
       {Object.keys(abilities).map((item) => (
         <Ability
-          abilityName={abilityDisplayNames[item]}
-          inputText={abilities[item]}
+          displayName={abilityDisplayNames[item]}
+          score={abilities[item]}
           abilities={abilities}
+          id={item}
           setAbilities={setAbilities}
-          abilityID={item}
           key={item}
         />
       ))}
