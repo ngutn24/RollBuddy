65a744d9321538f40ab663e06f42f6845bf31129
Arpan <arpans@cs.washington.edu>
2022-05-16 04:43:47 -0700

    CharClass JUnit tests added

--- Backend/src/main/java/CharClass.java
+++ Backend/src/main/java/CharClass.java
@@ -13,20 +13,24 @@ public class CharClass {
 
     /**
      * Initialize a ClassType object
-     * 
+     *
      * @param charClass The string name of the class
-     * @param level  The integer amount pertaining to the total levels in this class
+     * @param level     The integer amount pertaining to the total levels in this class
      */
     public CharClass(String charClass, int level) {
-        assert (!Objects.equals(charClass, "") && charClass != null)
-                : "You must have a main class type";
-        this.charClass = charClass;
-        this.level = level;
+        if (Objects.equals(charClass, "") || charClass == null) {
+            // In case of an undecided class (when too low level or not multiclass)
+            this.charClass = "Undecided";
+            this.level = 0;
+        } else {
+            this.charClass = charClass;
+            this.level = level;
+        }
     }
 
     /**
      * Get the name of the class
-     * 
+     *
      * @return A string name representing the class
      */
     public String getClassName() {
@@ -35,22 +39,28 @@ public class CharClass {
 
     /**
      * Get the class level
-     * 
+     *
      * @return An integer representing the class level
      */
-    public int getLevel() { return level; }
+    public int getLevel() {
+        return level;
+    }
 
     /**
      * Set the name of the class (SHOULD NEVER BE USED)
-     * 
+     *
      * @param charClass The new name of the class
      */
-    public void setClassName(String charClass) { this.charClass = charClass; }
+    public void setClassName(String charClass) {
+        this.charClass = charClass;
+    }
 
     /**
      * Set the level of the class
-     * 
+     *
      * @param level The new level of the class
      */
-    public void setLevel(int level) { this.level = level; }
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
