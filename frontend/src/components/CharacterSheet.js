7d569715e06939c1922fcdf7c09130343121440c
Tyler W <19383474+leftl@users.noreply.github.com>
2022-05-29 22:55:31 -0700

    Comment out unneeded code in CharacterSheet.

--- frontend/src/components/CharacterSheet.js
+++ frontend/src/components/CharacterSheet.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import AbilityList from "./AbilityList.js";
 import { Character } from "../character.js";
@@ -16,64 +16,64 @@ import CharacterInfo from "./CharacterInfo.js";
 import CharacterStatus from "./CharacterStatus.js";
 import RollMenu from "./RollMenu.js";
 import MiscMenu from "./MiscMenu.js";
-import axios from "axios";
+// import axios from "axios";
 import TabMenu from "./TabMenu.js";
 
 export const baseURL = process.env.REACT_APP_API_URL;
 
 const CharacterSheet = () => {
   // stores ID used to reference current character in *all* API calls.
-  const [characterID, setCharacterID] = useState();
+  // const [characterID, setCharacterID] = useState();
   const [charAttributes, setCharAttributes] = useState(Character); // default to uninitialize char
   const [charID, setCharID] = useState(); // default to uninitialize char
-  const [abilities, setAbilities] = useState({
-    STR: 10,
-    DEX: 10,
-    CON: 10,
-    INT: 10,
-    WIS: 10,
-    CHA: 10,
-  });
-  const isMounted = useRef(false); // used to skip effects on initial render.
+  // const [abilities, setAbilities] = useState({
+  //   STR: 10,
+  //   DEX: 10,
+  //   CON: 10,
+  //   INT: 10,
+  //   WIS: 10,
+  //   CHA: 10,
+  // });
+  // const isMounted = useRef(false); // used to skip effects on initial render.
 
-  // fetch character data from the API, will initialize a new character if not found.
-  const fetchCharacter = () => {
-    const characterID = JSON.parse(localStorage.getItem("characterID"));
+  // // fetch character data from the API, will initialize a new character if not found.
+  // const fetchCharacter = () => {
+  //   const characterID = JSON.parse(localStorage.getItem("characterID"));
 
-    let id = "";
-    if (characterID != null) {
-      setCharacterID(characterID);
-      id = "?id=" + characterID;
-    }
+  //   let id = "";
+  //   if (characterID != null) {
+  //     setCharacterID(characterID);
+  //     id = "?id=" + characterID;
+  //   }
 
-    // TODO: cleanup url construction/definition to ensure safety.
-    const url = encodeURI(baseURL + "/character" + id);
+  //   // TODO: cleanup url construction/definition to ensure safety.
+  //   const url = encodeURI(baseURL + "/character" + id);
 
-    console.log("fetching URL: ", url);
+  //   console.log("fetching URL: ", url);
 
-    axios
-      .get(url, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log(res.data);
+  //   axios
+  //     .get(url, {
+  //       withCredentials: true,
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
 
-        // set character ID if this call initializes a new character
-        if (characterID === null) {
-          console.log("Setting the chartacterID: ", res.data.id);
-          setCharacterID(res.data.id);
-        }
+  //       // set character ID if this call initializes a new character
+  //       if (characterID === null) {
+  //         console.log("Setting the chartacterID: ", res.data.id);
+  //         setCharacterID(res.data.id);
+  //       }
 
-        setAbilities({
-          STR: res.data.character.STR.abilityScore,
-          DEX: res.data.character.DEX.abilityScore,
-          CON: res.data.character.CON.abilityScore,
-          INT: res.data.character.INT.abilityScore,
-          WIS: res.data.character.WIS.abilityScore,
-          CHA: res.data.character.CHA.abilityScore,
-        });
-      });
-  };
+  //       setAbilities({
+  //         STR: res.data.character.STR.abilityScore,
+  //         DEX: res.data.character.DEX.abilityScore,
+  //         CON: res.data.character.CON.abilityScore,
+  //         INT: res.data.character.INT.abilityScore,
+  //         WIS: res.data.character.WIS.abilityScore,
+  //         CHA: res.data.character.CHA.abilityScore,
+  //       });
+  //     });
+  // };
 
   const anonAuthFetch = async () => {
     try {
@@ -126,31 +126,31 @@ const CharacterSheet = () => {
     }
   };
 
-  // post abilities to API, updating backend representation of the character.
-  const updateAbilities = () => {
-    if (characterID == null) {
-      console.log("[ERROR] No valid character ID found. Cannot update values.");
-      return;
-    }
+  // // post abilities to API, updating backend representation of the character.
+  // const updateAbilities = () => {
+  //   if (characterID == null) {
+  //     console.log("[ERROR] No valid character ID found. Cannot update values.");
+  //     return;
+  //   }
 
-    // TODO: cleanup url construction/definition to ensure safety.
-    let url = encodeURI(
-      baseURL +
-        "/update" +
-        "?id=" +
-        characterID +
-        "&stats=" +
-        JSON.stringify(abilities)
-    );
+  //   // TODO: cleanup url construction/definition to ensure safety.
+  //   let url = encodeURI(
+  //     baseURL +
+  //       "/update" +
+  //       "?id=" +
+  //       characterID +
+  //       "&stats=" +
+  //       JSON.stringify(abilities)
+  //   );
 
-    axios
-      .get(url, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log("[DEBUG] Updated character for session: ", res.data);
-      });
-  };
+  //   axios
+  //     .get(url, {
+  //       withCredentials: true,
+  //     })
+  //     .then((res) => {
+  //       console.log("[DEBUG] Updated character for session: ", res.data);
+  //     });
+  // };
 
   // Runs once: Initialize session, save id, and request initial data.
   useEffect(() => {
@@ -202,22 +202,22 @@ const CharacterSheet = () => {
     // eslint-disable-next-line
   }, []);
 
-  // when any ability updates, push to backend via API.
-  // TODO: there is room for optimization here. possibly batching and/or timers?
-  useEffect(() => {
-    if (isMounted.current) {
-      console.log("[DEBUG] Abilities after mount:", abilities);
-      updateAbilities();
-    } else {
-      isMounted.current = true;
-    }
-    // eslint-disable-next-line
-  }, [abilities]);
+  // // when any ability updates, push to backend via API.
+  // // TODO: there is room for optimization here. possibly batching and/or timers?
+  // useEffect(() => {
+  //   if (isMounted.current) {
+  //     console.log("[DEBUG] Abilities after mount:", abilities);
+  //     updateAbilities();
+  //   } else {
+  //     isMounted.current = true;
+  //   }
+  //   // eslint-disable-next-line
+  // }, [abilities]);
 
   // save characterID to localstorage when state is updated.
-  useEffect(() => {
-    localStorage.setItem("characterID", JSON.stringify(characterID));
-  }, [characterID]);
+  // useEffect(() => {
+  //   localStorage.setItem("characterID", JSON.stringify(characterID));
+  // }, [characterID]);
 
   //save characterID to localstorage when state is updated.
   useEffect(() => {
@@ -233,7 +233,10 @@ const CharacterSheet = () => {
   return (
     <Container style={{ padding: "20px" }}>
       <Row>
-        <CharacterInfo />
+        <CharacterInfo
+          attributes={charAttributes}
+          setAttributes={setCharAttributes}
+        />
       </Row>
       <Row>
         <CharacterStatus />
@@ -251,7 +254,7 @@ const CharacterSheet = () => {
               <MiscMenu />
             </Col>
             <Col>
-              <RollMenu id={characterID} />
+              <RollMenu id={charID} />
             </Col>
           </Row>
         </Col>
